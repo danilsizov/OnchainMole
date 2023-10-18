c86840c0512a787023e30aaa5026dc42fc84da6d
NikitaSizovV <sizov.space@gmail.com>
2023-10-18 23:12:18 +0300

    changed ui

--- client/src/components/SidebarMenu.vue
+++ client/src/components/SidebarMenu.vue
@@ -1,41 +1,96 @@
 <template>
   <div class="sidebar">
     <div class="sidebar_menu">
-      <div class="sidebar_menu_item active">Yield Aggregators</div>
-      <div class="sidebar_menu_item soon">DEX (Soon)</div>
-      <div class="sidebar_menu_item soon">Lending Protocol (Soon)</div>
-      <div class="sidebar_menu_item">Reports (0)</div>
+      <h2 class="logo">OnChainMole</h2>
+      <div class="sidebar_menu_item active">
+        <span>
+          Yield Aggregators
+        </span>
+      </div>
+      <div class="sidebar_menu_item soon">
+        <span>
+          DEX (Soon)
+        </span>
+      </div>
+      <div class="sidebar_menu_item soon">
+        <span>
+          Lending Protocol (Soon)
+        </span>
+      </div>
+      <div class="sidebar_menu_item">
+        <span>
+          Reports (0)
+        </span>
+      </div>
     </div>
   </div>
 </template>
 
-<style>
-  .sidebar{
-    height: calc(100vh - 90px);
-    width: 230px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    z-index: 1;
-    box-shadow: 0 0 45px 0 rgba(0,0,0,.6);
-    margin-top: 82px;
-    margin-left: 20px;
-    border-radius: 5px;
+<style lang="scss">
+.logo {
+  font-size: 32px;
+  color: white;
+  font-weight: 800;
+  font-family: 'Pixelify Sans';
+}
+
+.sidebar {
+  height: 100vh;
+  width: auto;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  // box-shadow: 0 0 45px 0 rgba(0, 0, 0, .6);
+  border-radius: 5px;
+  display: block;
+  background: #1F2336;
+  padding: 15px 15px 0 15px;
+
+  &::after {
     display: block;
-    background: linear-gradient(0deg,#389466,#42b883);
+    position: absolute;
+    top: 1%;
+    right: 0;
+    height: 98%;
+    background: #2c3148;
+    content: '';
+    width: 1px;
   }
-  .sidebar_menu {
-      font-family: Avenir, Helvetica, Arial, sans-serif;
-      -webkit-font-smoothing: antialiased;
-      -moz-osx-font-smoothing: grayscale;
-      text-align: left;
-      color: white;
-      min-height: 100%;
-      max-height: calc(100vh - 705px);
-      position: relative;
-      padding: 0px 10px 20px 10px;
+}
+
+.sidebar_menu {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  height: 100vh;
+  position: relative;
+  padding: 0px 10px 20px 10px;
+}
+
+.sidebar_menu_item {
+  padding: 20px 0;
+
+  span {
+    color: white;
   }
-  .sidebar_menu_item{
-    padding: 20px 0;
+
+  &.soon span {
+    cursor: default;
+    color: #767FA1;
+
+    &:hover {
+      color: #767FA1;
+    }
   }
+
+  span {
+    cursor: pointer;
+
+    &:hover {
+      color: #6D73FE;
+    }
+  }
+}
 </style>
--- client/src/components/TopMenu.vue
+++ client/src/components/TopMenu.vue
@@ -1,35 +1,38 @@
 <template>
   <div class="menu">
     <div class="logo">
-        OnChainMole
+      OnChainMole
     </div>
     <div class="wallet_connect">
-        Connect Wallet
+      Connect Wallet
     </div>
   </div>
 </template>
 
 <style>
-  .menu{
-    width: 100%;
-    height: 60px;
-    display: flex;
-    box-sizing: border-box;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
-  }
-  .logo{
-    font-size: 20px;
-    color: white;
-    font-weight: 800;
-  }
-  .wallet_connect{
-    padding: 7px 15px;
-    background: linear-gradient(0deg,#389466,#42b883);
-    color: white;
-    border-radius: 50px;
-    cursor: pointer;
-    font-weight: 600;
-  }
+@import url('https://fonts.googleapis.com/css2?family=Pixelify+Sans&family=Poppins:wght@300;600&display=swap');
+
+.menu {
+  width: 100%;
+  height: 70px;
+  display: flex;
+  box-sizing: border-box;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 45px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: #1F2336;
+}
+
+.wallet_connect {
+  padding: 7px 15px;
+  background: rgb(70, 83, 211);
+  background: linear-gradient(45deg, rgba(70, 83, 211, 1) 0%, rgba(73, 159, 206, 1) 78%);
+  color: white;
+  border-radius: 50px;
+  cursor: pointer;
+  font-weight: 600;
+}
 </style>
