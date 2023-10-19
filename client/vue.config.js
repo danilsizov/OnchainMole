79cf869c34675d5d75e05cd943777c34cbdb45ae
NikitaSizovV <sizov.space@gmail.com>
2023-10-19 14:34:46 +0300

    droplet

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  devServer: {
-    host: '0.0.0.0',
-    disableHostCheck: true
-  }
+  transpileDependencies: true
 })
